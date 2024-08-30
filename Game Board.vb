9940f66001c3a93ae9b015027e71000c64bc19ff
chuxueyi <chuxueyi@126.com>
2024-08-30 19:42:56 +0800

    still have problem with recursion

--- Game Board.vb
+++ Game Board.vb
@@ -1,55 +1,57 @@
 ﻿Public Class frmGame
 
+    'icon-size image paths
     Dim stPathSmiley = "C:\Users\Administrator\source\repos\MineSweeper\Resources\smiley1.ico"
     Dim stPathFeelingGood = "C:\Users\Administrator\source\repos\MineSweeper\Resources\smiley.ico"
     Dim stPathNotHappy = "C:\Users\Administrator\source\repos\MineSweeper\Resources\smiley3.ico"
     Dim stPathExploded = "C:\Users\Administrator\source\repos\MineSweeper\Resources\mine4.ico"
     Dim stPathDiscovered = "C:\Users\Administrator\source\repos\MineSweeper\Resources\mine2.ico"
 
-
+    'x axis / columns
     Dim iC = Form1.iCol
+    'y axis / rows
     Dim iR = Form1.iRow
+    'total button numbers
     Dim iCR = iC * iR
 
+    'hidden answer board
+    Dim aiHint(iCR - 1) As Integer     'last index in parenthesis
 
-    Dim aiHint(iCR) As Integer
+    'fogged, discovered, or safe
+    Dim astState(iCR - 1) As String
 
+    'every surrounding buttons' indices for every button
+    Dim aprSrdg(iCR - 1) As (Integer, Integer())
 
+    'number of mines discovered successfully
     Dim iDscvrdToTal As Integer
 
-
+    'load the game board according to designated sizes
     Private Sub frmGame_Load() Handles MyBase.Load
 
-
+        'add the start happy face picturebox
         Me.Controls.Add(New PictureBox With {
             .Image = Image.FromFile(stPathSmiley),
-            .Width = iC * 30 \ 6,
-            .Height = iC * 30 \ 6,
-            .SizeMode = PictureBoxSizeMode.StretchImage,
-            .BorderStyle = BorderStyle.None,
-            .Location = New Point((iC * 30 - iC * 30 \ 6) / 2, iC)})
+            .Width = 50,
+            .Height = 50,
+            .SizeMode = PictureBoxSizeMode.StretchImage,    'stretch the image so it's size is the same as the picbox
+        .Location = New Point((iC * 30 - 50) / 2, 30)})    'center horizontally, down a bit vertically
 
-
-
-        Dim iX = 0
-        Dim iY = iC * 30 \ 6 + 2 * iC
+        'start x of game board, 0
+        'start y of game board, 50 + 2 * 30
+        Dim sy = 110
 
         Dim i = 0
 Board:  Me.Controls.Add(New Button With {
+            .Name = CStr(i),     'index in me.controls needs plus 1
             .Text = "",
+            .Image = Nothing,
             .Width = 30,
             .Height = 30,
             .Margin = New Padding(0),
-            .Location = New Point(iX + (i Mod iC) * 30, iY + (i \ iC) * 30)})
+            .Location = New Point((i Mod iC) * 30, sy + (i \ iC) * 30)})
         i += 1
-        If i < iC * iR Then GoTo Board
-
-
-        Me.Controls.SetChildIndex(Me.Controls.Item(0), iCR + 1)
-        For index = 1 To iCR
-            Me.Controls.SetChildIndex(Me.Controls.Item(i), i - 1)
-        Next
-        Me.Controls.SetChildIndex(Me.Controls.Item(0), iCR)
+        If i < iCR Then GoTo Board
 
 
         For Each ctrl In Me.Controls
@@ -58,9 +60,9 @@ Board:  Me.Controls.Add(New Button With {
                 AddHandler DirectCast(ctrl, Button).Click, Sub(sender As Button, e As EventArgs)
 
 
-                                                               Dim pvt As Integer = Me.Controls.GetChildIndex(ctrl)
+                                                               Dim pvt As Integer = sender.Name
 
-
+                                                               MsgBox(pvt)
                                                                If aiHint(pvt) = -1 Then
 
                                                                    sender.Image = Image.FromFile(stPathExploded)
@@ -74,6 +76,7 @@ Board:  Me.Controls.Add(New Button With {
                                                                If aiHint(pvt) <> -1 Then
 
                                                                    sender.Text = aiHint(pvt)
+                                                                   astState(pvt) = "safe"
                                                                    SweepSurrounding(pvt)
 
                                                                End If
@@ -98,92 +101,74 @@ Board:  Me.Controls.Add(New Button With {
 
     Private Sub SweepSurrounding(pvt As Integer)
 
-
-        Dim srdgIdcs() = SurroundingIndices(pvt)
-        Dim sCount = srdgIdcs.Count
-        Dim state(sCount - 1) As String
-
-        Dim fogged = 0
-        Dim discovered = 0
-        Dim safe = 0
-
-        For index = 0 To sCount - 1
-            Dim si = srdgIdcs(index)
-            Dim stt = state(si)
-            Dim sBtn As Button = Me.Controls.Item(si + 1)
-
-            If sBtn.Text = "" And sBtn.Image Is Nothing Then
-                fogged += 1
-                stt = "fogged"
-            End If
-            If sBtn.Image IsNot Nothing Then     'image or text
-                discovered += 1
-                stt = "discovered"
-            End If
-            If sBtn.Text <> "" Then
-                safe += 1
-                stt = "safe"
-            End If
-
-        Next
-
-
         Dim pvtHint = aiHint(pvt)
+        Dim srdgIdcs() = aprSrdg(pvt).Item2
 
-
-        If discovered = pvtHint Then
-
-            If fogged = 0 Then Exit Sub      'recursion exit
-
-            If fogged > 0 Then
-                For index = 0 To sCount - 1
-                    If state(index) = "fogged" Then Me.Controls.Item(srdgIdcs(index) + 1).Text = aiHint(srdgIdcs(index))
-                Next
-                Exit Sub           'recursion exit
-            End If
-
+        If pvtHint = 0 Then
+            For Each si In srdgIdcs
+                Me.Controls.Item(si + 1).Text = aiHint(si)
+                astState(si) = "safe"
+            Next
         End If
 
-
-        If discovered < pvtHint Then
-
-            If discovered + fogged > pvtHint Then
-                Exit Sub    'still can work
-            End If
-
-            If discovered + fogged = pvtHint Then
-                For index = 0 To sCount - 1
-                    If state(index) = "fogged" Then DirectCast(Me.Controls.Item(srdgIdcs(index) + 1), Button).Image = Image.FromFile(stPathDiscovered)
-                Next
-                iDscvrdToTal += 1
-                If iDscvrdToTal = Form1.iMines Then DirectCast(Me.Controls.Item(0), PictureBox).Image = Image.FromFile(stPathFeelingGood)
-                Exit Sub    'recursion exit
-            End If
-
+        If pvtHint = 8 Then
+            For Each si In srdgIdcs
+                DirectCast(Me.Controls.Item(si + 1), Button).Image = Image.FromFile(stPathDiscovered)
+                astState(si) = "discovered"
+            Next
         End If
 
+        If pvtHint > 0 And pvtHint < 8 Then
+            Dim fogged = 0
+            Dim discovered = 0
+            For Each si In srdgIdcs
+                If astState(si) = "fogged" Then fogged += 1
+                If astState(si) = "discovered" Then discovered += 1
+            Next
+            If discovered = pvtHint Then
+                If fogged > 0 Then
+                    For Each si In srdgIdcs
+                        If astState(si) = "fogged" Then Me.Controls.Item(si + 1).Text = aiHint(si)
+                        astState(si) = "safe"
+                    Next
+                End If
+            End If
+            If discovered < pvtHint Then
+                If discovered + fogged > pvtHint Then Exit Sub   'recursion exit on needing people
+                If discovered + fogged = pvtHint Then
+                    For Each si In srdgIdcs
+                        If astState(si) = "fogged" Then DirectCast(Me.Controls.Item(si + 1), Button).Image = Image.FromFile(stPathDiscovered)
+                        astState(si) = "discovered"
+                        iDscvrdToTal += 1
+                        If iDscvrdToTal = Form1.iMines Then
+                            DirectCast(Me.Controls.Item(0), PictureBox).Image = Image.FromFile(stPathFeelingGood)
+                            Exit Sub    'recursion exit on winning
+                        End If
+                    Next
+                End If
+            End If
+        End If
 
-        For Each si In srdgIdcs
-            If Me.Controls.Item(si + 1).Text <> "" Then SweepSurrounding(si)
-        Next
+        'For Each si In srdgIdcs
+        '    If astState(si) = "safe" Then SweepSurrounding(si)
+        'Next
 
 
     End Sub
 
     Private Sub NewGame()
 
-
-        For Each hint In aiHint
-            hint = 0
+        For Each h In aiHint
+            h = 0
         Next
-
 
         iDscvrdToTal = 0
 
-
         For index = 1 To iCR
             Me.Controls.Item(index).Text = ""
-            DirectCast(Me.Controls.Item(index), Button).Image = Nothing
+            Me.Controls.Item(index).Image = Nothing
+            astState(index - 1) = "fogged"
+            aprSrdg(index - 1) = (index - 1, SurroundingIndices(index - 1))
         Next
 
 
@@ -197,19 +182,17 @@ Random:     Dim t = Int(iCR * Rnd())
         Next
 
 
-
         For index = 0 To iCR - 1
 
             If aiHint(index) = 0 Then
 
                 Dim count = 0
 
-                Dim srdgIndices() = SurroundingIndices(index)
-                For Each si In srdgIndices
+                For Each si In aprSrdg(index).Item2
                     If aiHint(si) = -1 Then count += 1
                 Next
 
-                aiHint(index) = count       'update hint
+                aiHint(index) = count
 
             End If
 
@@ -220,23 +203,60 @@ Random:     Dim t = Int(iCR * Rnd())
     End Sub
 
 
-
     Private Function SurroundingIndices(i As Integer) As Integer()
 
-        Dim srdgIdcs() As Integer = {}
+        Dim srdg8(7) As Integer
 
-        If i - 1 >= 0 And i - 1 < iCR And i Mod iC <> 0 Then srdgIdcs.Append(i - 1)
-        If i + 1 >= 0 And i + 1 < iCR And i Mod iC <> iC - 1 Then srdgIdcs.Append(i + 1)
-        If i - iC >= 0 And i - iC < iCR And i \ iC <> 0 Then srdgIdcs.Append(i - iC)
-        If i + iC >= 0 And i + iC < iCR And i \ iC <> iR - 1 Then srdgIdcs.Append(i + iC)
-        If i - iC - 1 >= 0 And i - iC - 1 < iCR And i Mod iC <> 0 And i \ iC <> 0 Then srdgIdcs.Append(i - iC - 1)
-        If i + iC - 1 >= 0 And i + iC - 1 < iCR And i Mod iC <> 0 And i \ iC <> iR - 1 Then srdgIdcs.Append(i + iC - 1)
-        If i - iC + 1 >= 0 And i - iC + 1 < iCR And i Mod iC <> iC - 1 And i \ iC <> 0 Then srdgIdcs.Append(i - iC + 1)
-        If i + iC + 1 >= 0 And i + iC + 1 < iCR And i Mod iC <> iC - 1 And i \ iC <> iR - 1 Then srdgIdcs.Append(i + iC + 1)
+        Dim srdgCount = 0
+
+        If i - 1 >= 0 And i - 1 < iCR And i Mod iC <> 0 Then
+            srdg8(srdgCount) = i - 1
+            srdgCount += 1
+        End If
+        If i + 1 >= 0 And i + 1 < iCR And i Mod iC <> iC - 1 Then
+            srdg8(srdgCount) = i + 1
+            srdgCount += 1
+        End If
+        If i - iC >= 0 And i - iC < iCR And i \ iC <> 0 Then
+            srdg8(srdgCount) = i - iC
+            srdgCount += 1
+        End If
+        If i + iC >= 0 And i + iC < iCR And i \ iC <> iR - 1 Then
+            srdg8(srdgCount) = i + iC
+            srdgCount += 1
+        End If
+        If i - iC - 1 >= 0 And i - iC - 1 < iCR And i Mod iC <> 0 And i \ iC <> 0 Then
+            srdg8(srdgCount) = i - iC - 1
+            srdgCount += 1
+        End If
+        If i + iC - 1 >= 0 And i + iC - 1 < iCR And i Mod iC <> 0 And i \ iC <> iR - 1 Then
+            srdg8(srdgCount) = i + iC - 1
+            srdgCount += 1
+        End If
+        If i - iC + 1 >= 0 And i - iC + 1 < iCR And i Mod iC <> iC - 1 And i \ iC <> 0 Then
+            srdg8(srdgCount) = i - iC + 1
+            srdgCount += 1
+        End If
+        If i + iC + 1 >= 0 And i + iC + 1 < iCR And i Mod iC <> iC - 1 And i \ iC <> iR - 1 Then
+            srdg8(srdgCount) = i + iC + 1
+            srdgCount += 1
+        End If
+
+        Dim srdgIdcs(srdgCount - 1) As Integer
+        For index = 0 To srdgCount - 1
+            srdgIdcs(index) = srdg8(index)
+        Next
+
 
         Return srdgIdcs
 
     End Function
+
+    Private Sub Repeat(times As Integer, oneTime As Func(Of Object))
+        For index = 1 To times
+            oneTime()
+        Next
+    End Sub
 
 
 
