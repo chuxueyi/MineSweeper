aba5261bc9e0392c608aeca30e96d967b3111aae
chuxueyi <chuxueyi@126.com>
2024-08-31 17:31:38 +0800

    hints are right something wrong with probability

--- Game Board.vb
+++ Game Board.vb
@@ -1,11 +1,12 @@
 ﻿Public Class frmGame
 
-    'icon-size image paths
+    'gaming
     Dim stPathSmiley = "C:\Users\Administrator\source\repos\MineSweeper\Resources\smiley1.ico"
+    'winning
     Dim stPathFeelingGood = "C:\Users\Administrator\source\repos\MineSweeper\Resources\smiley.ico"
+    'losing
     Dim stPathNotHappy = "C:\Users\Administrator\source\repos\MineSweeper\Resources\smiley3.ico"
     Dim stPathExploded = "C:\Users\Administrator\source\repos\MineSweeper\Resources\mine4.ico"
-    Dim stPathDiscovered = "C:\Users\Administrator\source\repos\MineSweeper\Resources\mine2.ico"
 
     'x axis / columns
     Dim iC = Form1.iCol
@@ -17,14 +18,16 @@
     'hidden answer board
     Dim aiHint(iCR - 1) As Integer     'last index in parenthesis
 
-    'fogged, discovered, or safe
-    Dim astState(iCR - 1) As String
+    'mine probability from player's point of view
+    Dim aiProb(iCR - 1) As Integer
 
-    'every surrounding buttons' indices for every button
+    'every surrounding-buttons-indices for every button
     Dim aprSrdg(iCR - 1) As (Integer, Integer())
 
-    'number of mines discovered successfully
-    Dim iDscvrdToTal As Integer
+    '
+    'winning
+    '
+
 
     'load the game board according to designated sizes
     Private Sub frmGame_Load() Handles MyBase.Load
@@ -35,21 +38,19 @@
             .Width = 50,
             .Height = 50,
             .SizeMode = PictureBoxSizeMode.StretchImage,    'stretch the image so it's size is the same as the picbox
-        .Location = New Point((iC * 30 - 50) / 2, 30)})    'center horizontally, down a bit vertically
+        .Location = New Point((iC * 50 - 50) / 2, 25)})    'center horizontally, down a bit vertically
 
-        'start x of game board, 0
-        'start y of game board, 50 + 2 * 30
-        Dim sy = 110
 
         Dim i = 0
 Board:  Me.Controls.Add(New Button With {
             .Name = CStr(i),     'index in me.controls needs plus 1
             .Text = "",
             .Image = Nothing,
-            .Width = 30,
-            .Height = 30,
+            .TextAlign = ContentAlignment.MiddleCenter,
+            .Width = 50,
+            .Height = 50,
             .Margin = New Padding(0),
-            .Location = New Point((i Mod iC) * 30, sy + (i \ iC) * 30)})
+            .Location = New Point((i Mod iC) * 50, 100 + (i \ iC) * 50)})
         i += 1
         If i < iCR Then GoTo Board
 
@@ -62,13 +63,10 @@ Board:  Me.Controls.Add(New Button With {
 
                                                                Dim pvt As Integer = sender.Name
 
-                                                               MsgBox(pvt)
                                                                If aiHint(pvt) = -1 Then
-
+                                                                   sender.Text = ""
                                                                    sender.Image = Image.FromFile(stPathExploded)
                                                                    DirectCast(Me.Controls.Item(0), PictureBox).Image = Image.FromFile(stPathNotHappy)
-                                                                   'MsgBox("Game End! Swept: " & iDscvrdToTal * 100 \ Form1.iMines & "%")
-                                                                   Exit Sub
 
                                                                End If
 
@@ -76,8 +74,9 @@ Board:  Me.Controls.Add(New Button With {
                                                                If aiHint(pvt) <> -1 Then
 
                                                                    sender.Text = aiHint(pvt)
-                                                                   astState(pvt) = "safe"
-                                                                   SweepSurrounding(pvt)
+                                                                   aiProb(pvt) = 0
+
+                                                                   UpdateProb(pvt)
 
                                                                End If
 
@@ -97,77 +96,15 @@ Board:  Me.Controls.Add(New Button With {
 
     End Sub
 
-
-
-    Private Sub SweepSurrounding(pvt As Integer)
-
-        Dim pvtHint = aiHint(pvt)
-        Dim srdgIdcs() = aprSrdg(pvt).Item2
-
-        If pvtHint = 0 Then
-            For Each si In srdgIdcs
-                Me.Controls.Item(si + 1).Text = aiHint(si)
-                astState(si) = "safe"
-            Next
-        End If
-
-        If pvtHint = 8 Then
-            For Each si In srdgIdcs
-                DirectCast(Me.Controls.Item(si + 1), Button).Image = Image.FromFile(stPathDiscovered)
-                astState(si) = "discovered"
-            Next
-        End If
-
-        If pvtHint > 0 And pvtHint < 8 Then
-            Dim fogged = 0
-            Dim discovered = 0
-            For Each si In srdgIdcs
-                If astState(si) = "fogged" Then fogged += 1
-                If astState(si) = "discovered" Then discovered += 1
-            Next
-            If discovered = pvtHint Then
-                If fogged > 0 Then
-                    For Each si In srdgIdcs
-                        If astState(si) = "fogged" Then Me.Controls.Item(si + 1).Text = aiHint(si)
-                        astState(si) = "safe"
-                    Next
-                End If
-            End If
-            If discovered < pvtHint Then
-                If discovered + fogged > pvtHint Then Exit Sub   'recursion exit on needing people
-                If discovered + fogged = pvtHint Then
-                    For Each si In srdgIdcs
-                        If astState(si) = "fogged" Then DirectCast(Me.Controls.Item(si + 1), Button).Image = Image.FromFile(stPathDiscovered)
-                        astState(si) = "discovered"
-                        iDscvrdToTal += 1
-                        If iDscvrdToTal = Form1.iMines Then
-                            DirectCast(Me.Controls.Item(0), PictureBox).Image = Image.FromFile(stPathFeelingGood)
-                            Exit Sub    'recursion exit on winning
-                        End If
-                    Next
-                End If
-            End If
-        End If
-
-        'For Each si In srdgIdcs
-        '    If astState(si) = "safe" Then SweepSurrounding(si)
-        'Next
-
-
-    End Sub
-
     Private Sub NewGame()
 
-        For Each h In aiHint
-            h = 0
-        Next
+        aiHint.Initialize()
 
-        iDscvrdToTal = 0
 
         For index = 1 To iCR
-            Me.Controls.Item(index).Text = ""
+            aiProb(index - 1) = 100
+            Me.Controls.Item(index).Text = aiProb(index - 1) & "%"
             Me.Controls.Item(index).Image = Nothing
-            astState(index - 1) = "fogged"
             aprSrdg(index - 1) = (index - 1, SurroundingIndices(index - 1))
         Next
 
@@ -202,6 +139,33 @@ Random:     Dim t = Int(iCR * Rnd())
 
     End Sub
 
+    Private Sub UpdateProb(startIndex As Integer)
+
+        If aiHint(startIndex) = 0 Then
+            For Each si In aprSrdg(startIndex).Item2
+                Me.Controls.Item(si + 1).Text = aiHint(si)
+                aiProb(si) = 0
+            Next
+        End If
+        If aiHint(startIndex) <> 0 Then
+            Dim count = 0
+            For Each si As Integer In aprSrdg(startIndex).Item2
+                If aiProb(si) <> 0 Then count += 1
+            Next
+            If count <> 0 Then
+                For Each si As Integer In aprSrdg(startIndex).Item2
+                    If aiProb(si) <> 0 Then
+                        aiProb(si) = aiHint(startIndex) * 100 \ count
+                        Me.Controls.Item(si).Text = aiProb(si) & "%"
+                    End If
+
+                Next
+            End If
+
+        End If
+
+
+    End Sub
 
     Private Function SurroundingIndices(i As Integer) As Integer()
 
